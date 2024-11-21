587293ad5463d82fe601745d5b4e0a613c041286
Kirman442 <kirill@irminlabs.com>
2024-11-21 18:57:18 +0100

    add: some fixes

--- src/main.jsx
+++ src/main.jsx
@@ -13,13 +13,13 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
+    <HashRouter
       future={{
         v7_startTransition: true,
         v7_relativeSplatPath: true,
       }}
     >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
