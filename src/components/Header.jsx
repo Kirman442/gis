6042ca7cad04b81ccc3d52c9f85d9dddd095eeae
Kirman442 <kirill@irminlabs.com>
2024-11-21 21:58:50 +0100

    add: some fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
 
 const getMenuItems = (pathname) => {
@@ -36,11 +36,47 @@ const getMenuItems = (pathname) => {
 
 const Header = () => {
     const location = useLocation();
+    const [isVisible, setIsVisible] = useState(true); // Показывать/скрывать меню
+    const [lastScrollY, setLastScrollY] = useState(0); // Последняя позиция скролла
+
+    // Логика скроллинга
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+        // console.log(`Current Scroll: ${currentScrollY}, Last Scroll: ${lastScrollY}`);
+
+        if (currentScrollY > lastScrollY && currentScrollY > 50) {
+            // console.log("Scrolling down: hiding header");
+            setIsVisible(false);
+        } else {
+            // console.log("Scrolling up: showing header");
+            setIsVisible(true);
+        }
+
+        setLastScrollY(currentScrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        // console.log("Scrolling...", window.scrollY);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
+
 
     return (
-        <header>
+        <header
+            style={{
+                position: "fixed",
+                top: 0,
+                width: "100%",
+                zIndex: 1000,
+                transform: isVisible ? "translateY(0)" : "translateY(-100%)", // Плавное скрытие
+                transition: "transform 0.3s ease", // Плавная анимация
+            }}
+        >
             {/* --start navigation -- */}
-            <nav className="navbar navbar-default bootsnav navbar-top header-light background-transparent nav-box-width navbar-expand-lg">
+            <nav className="navbar navbar-default bootsnav header-light background-transparent nav-box-width navbar-expand-lg"> {/* navbar-top */}
                 <div className="container-fluid nav-header-container">
                     {/* Mobile menu button */}
                     <div className="col accordion-menu pe-0 pe-md-3">
