540e97f7df80d8ceed9d857cb60f0d1a13f413c7
Kirman442 <kirill@irminlabs.com>
2024-11-23 17:46:03 +0100

    add: some fixes for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/react-gis/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/gis/" : "/",
   build: {
     chunkSizeWarningLimit: 1000,
   }
