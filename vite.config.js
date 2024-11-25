58e37a6116f6a877fd912c282df1ed131d40c8ad
Kirman442 <kirill@irminlabs.com>
2024-11-25 21:37:52 +0100

    support for css js minifying and fonts

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  cssInjectedByJsPlugin(),
+  ViteImageOptimizer({
+    png: {
+      quality: 80,
+    },
+    jpeg: {
+      quality: 75,
+    },
+    webp: {
+      quality: 75,
+    },
+  }),
+  ],
   base: process.env.NODE_ENV === "production" ? "/gis/" : "/",
   build: {
     chunkSizeWarningLimit: 1000,
+    cssMinify: true,
+    cssCodeSplit: false,
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          const names = assetInfo.names || [];
+          const isFontFile = names.some(name =>
+            /\.(woff2?|ttf|otf|eot)$/.test(name)
+          );
+
+          if (isFontFile) {
+            // Сохраняем шрифты в определенной структуре
+            return 'fonts/[name][extname]';
+          }
+
+          return 'assets/[name]-[hash][extname]';
+        }
+      },
+    },
   },
   optimizeDeps: {
     exclude: ['js-big-decimal']
