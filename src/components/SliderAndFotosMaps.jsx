4c363beff83fd9b6cf0898960bca38c575231bd9
Kirman442 <kirill@irminlabs.com>
2024-11-25 21:33:57 +0100

    add webp support for pages and gallerys

--- src/components/SliderAndFotosMaps.jsx
+++ src/components/SliderAndFotosMaps.jsx
@@ -1,41 +1,48 @@
+const isWebpSupported = () => {
+    const elem = document.createElement('canvas');
+    return !!(elem.getContext && elem.getContext('2d')) && elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+};
+
+const webpSupported = isWebpSupported();
+
 export const PortfolioSliderFotos = [
     {
-        image: `${import.meta.env.BASE_URL}images/about/aboutPekin1000.jpg`,
+        image: `${import.meta.env.BASE_URL}images/about/aboutPekin1000.${webpSupported ? 'webp' : 'jpg'}`,
         link: '/portfolio/Kunstkarten',
         header: 'Kunstkarten mit Python erstellen',
         text: 'Programmierung, Datenbearbeitung, Visualisierung',
         id: 1
     },
     {
-        image: `${import.meta.env.BASE_URL}images/about/aboutFluss1000.jpg`,
+        image: `${import.meta.env.BASE_URL}images/about/aboutFluss1000.${webpSupported ? 'webp' : 'jpg'}`,
         link: '/portfolio/Flusssystem',
         header: 'Die Visualisierung des Flusssystems',
         text: 'Programmierung, Datenbearbeitung',
         id: 2
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/brabutor.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/brabutor.${webpSupported ? 'webp' : 'jpg'}`,
         link: '/portfolio/Ubahn',
         header: 'Berliner U-Bahn',
         text: 'Programmierung, Datenbearbeitung',
         id: 3
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/risikomanagement.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/risikomanagement.${webpSupported ? 'webp' : 'jpg'}`,
         link: '/portfolio/Bergbau',
         header: 'Risikomanagement oberflächennaher Strukturen',
         text: 'Programmierung, Datenbearbeitung, 3D-Modellierung',
         id: 4
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/clustering2.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/clustering2.${webpSupported ? 'webp' : 'jpg'}`,
         link: '/portfolio/Clustering',
         header: 'Clustering',
         text: 'Programmierung, Datenbearbeitung, Web-App',
         id: 5
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/Dresden-hc.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/Dresden-hc.${webpSupported ? 'webp' : 'jpg'}`,
         link: '/portfolio/Dresden',
         header: 'Infrastruktur um Dresden',
         text: 'Programmierung, Datenbearbeitung, Web-App',
@@ -45,18 +52,18 @@ export const PortfolioSliderFotos = [
 
 export const KunstkartenOldImages = [
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Dortmund_1.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Dortmund_1.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Dortmund_1.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Dortmund_1.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Dortmund',
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Koln_1.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Koln_1.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Koln_1.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Koln_1.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Köln",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Paris_1.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Paris_1.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Paris_1.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/city-of-Paris_1.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Paris",
     }
 ];
@@ -64,43 +71,43 @@ export const KunstkartenOldImages = [
 
 export const KunstkartenNewImages = [
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Dortmund_final_9500_211011.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Dortmund_final_9500_211011.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Dortmund_final_9500_211011.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Dortmund_final_9500_211011.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Dortmund',
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Tripoly_final_12500_20052.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Tripoly_final_12500_20052.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Tripoly_final_12500_20052.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Tripoly_final_12500_20052.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Tripoli"
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Dubai_final_12500_211042.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Dubai_final_12500_211042.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Dubai_final_12500_211042.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Dubai_final_12500_211042.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Dubai",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Ho-Chi-Minh_final_7000_211042.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Ho-Chi-Minh_final_7000_211042.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Ho-Chi-Minh_final_7000_211042.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Ho-Chi-Minh_final_7000_211042.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Ho-Chi-Minh",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Istanbul_final_19500_211042.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Istanbul_final_19500_211042.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Istanbul_final_19500_211042.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Istanbul_final_19500_211042.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Istanbul",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Moscow_final_19000_211042.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Moscow_final_19000_211042.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Moscow_final_19000_211042.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Moscow_final_19000_211042.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Moscow",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Paris_final_19500_111052.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Paris_final_19500_111052.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Paris_final_19500_111052.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Paris_final_19500_111052.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Paris",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/kunstkarten/Stockholm_final_7500_20112.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Stockholm_final_7500_20112.jpg`,
+        src: `${import.meta.env.BASE_URL}images/kunstkarten/Stockholm_final_7500_20112.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/kunstkarten/Stockholm_final_7500_20112.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Stockholm",
     }
 ];
@@ -135,7 +142,7 @@ export const AboutSkills = [
 
 export const HomeProjectFotos = [
     {
-        image: `${import.meta.env.BASE_URL}images/home/Pekin_1_1_1_0_5_1.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/Pekin_1_1_1_0_5_1.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Kunstkarten',
         data: '18 April 2024',
         link: '/portfolio/Kunstkarten',
@@ -144,7 +151,7 @@ export const HomeProjectFotos = [
         id: 1
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/Fluss_Germany.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/Fluss_Germany.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Flusssystemstkarten',
         data: '25 Mai 2024',
         link: '/portfolio/Flusssystem',
@@ -153,7 +160,7 @@ export const HomeProjectFotos = [
         id: 2
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/brabutor.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/brabutor.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Berliner U-Bahn Geschichte',
         data: '19 Dezember 2023',
         link: '/portfolio/Ubahn',
@@ -162,7 +169,7 @@ export const HomeProjectFotos = [
         id: 3
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/risikomanagement.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/risikomanagement.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Bergbau Risikomanagement',
         data: '25 August 2022 | Kirill Lisochenko & Martin Burzynski',
         link: '/portfolio/Bergbau',
@@ -171,7 +178,7 @@ export const HomeProjectFotos = [
         id: 4
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/clustering2.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/clustering2.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Infrastruktur um Dresden',
         data: '25 März 2024',
         link: '/portfolio/Clustering',
@@ -180,7 +187,7 @@ export const HomeProjectFotos = [
         id: 5
     },
     {
-        image: `${import.meta.env.BASE_URL}images/home/Dresden-hc.jpg`,
+        image: `${import.meta.env.BASE_URL}images/home/Dresden-hc.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'Clustering',
         data: '25 September 2023',
         link: '/portfolio/Dresden',
@@ -193,28 +200,28 @@ export const HomeProjectFotos = [
 
 export const FlusssystemImages = [
     {
-        src: `${import.meta.env.BASE_URL}images/flusssystem/France.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/flusssystem/France.jpg`,
+        src: `${import.meta.env.BASE_URL}images/flusssystem/France.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/flusssystem/France.${webpSupported ? 'webp' : 'jpg'}`,
         alt: 'France',
     },
     {
-        src: `${import.meta.env.BASE_URL}images/flusssystem/Germany.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/flusssystem/Germany.jpg`,
+        src: `${import.meta.env.BASE_URL}images/flusssystem/Germany.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/flusssystem/Germany.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Germany"
     },
     {
-        src: `${import.meta.env.BASE_URL}images/flusssystem/Uruguay.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/flusssystem/Uruguay.jpg`,
+        src: `${import.meta.env.BASE_URL}images/flusssystem/Uruguay.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/flusssystem/Uruguay.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Uruguay",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/flusssystem/Venezuela.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/flusssystem/Venezuela.jpg`,
+        src: `${import.meta.env.BASE_URL}images/flusssystem/Venezuela.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/flusssystem/Venezuela.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "Venezuela",
     },
     {
-        src: `${import.meta.env.BASE_URL}images/flusssystem/South_America_upland50_ord_flow6.jpg`,
-        thumb: `${import.meta.env.BASE_URL}images/flusssystem/South_America_upland50_ord_flow6.jpg`,
+        src: `${import.meta.env.BASE_URL}images/flusssystem/South_America_upland50_ord_flow6.${webpSupported ? 'webp' : 'jpg'}`,
+        thumb: `${import.meta.env.BASE_URL}images/flusssystem/South_America_upland50_ord_flow6.${webpSupported ? 'webp' : 'jpg'}`,
         alt: "South America",
     }
 ];
